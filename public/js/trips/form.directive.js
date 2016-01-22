3def738a6f63cbdd7256cf95db685ae4a1298eb3
Chase Gruber <chasegruber@gmail.com>
2016-01-21 21:28:05 -0500

    create trips through form directive

---
+++ public/js/trips/form.directive.js
@@ -0,0 +1,40 @@
+"use strict";
+
+(function(){
+  angular
+  .module("trips")
+  .directive("tripForm", [
+    "TripFactory",
+    "$state",
+    TripFormDirectiveFunction
+  ]);
+
+  function TripFormDirectiveFunction(TripFactory, $state){
+    return{
+      templateUrl: "js/trips/_form.html",
+      scope: {
+        trip: "="
+      },
+      link: function(scope){
+        scope.create = function(){
+          scope.trip.$save(function(response){
+            $state.go("tripsShow", {id: response._id}, {reload: true});
+          });
+        }
+        scope.update = function(){
+          scope.trip.$update({id: scope.trip._id}, function(response){
+            $state.go("tripsShow", {id: response._id}, {reload: true});
+          });
+        }
+        scope.delete = function(){
+          scope.trip.$delete({id: scope.trip._id}, function(){
+            $state.go("tripsIndex", {}, {reload: true});
+          });
+        }
+        scope.cancel = function(){
+          $state.go("tripsShow", {id: response._id}, {reload: true});
+        }
+      }
+    }
+  }
+}());

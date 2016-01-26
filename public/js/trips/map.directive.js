b9fac9b1c2575cd2d8943b183300c981204c26f8
Chase Gruber <chasegruber@gmail.com>
2016-01-26 09:43:52 -0500

    geocoder in map directive

--- public/js/trips/map.directive.js
+++ public/js/trips/map.directive.js
@@ -16,11 +16,23 @@
         trip: "="
       },
       link: function(scope){
-        // console.log(scope)
         L.mapbox.accessToken = 'pk.eyJ1IjoiY2hhc2VncnViZXIiLCJhIjoidV9tdHNYSSJ9.RRyvDLny4YwDwzPCeOJZrA';
         var map = L.mapbox.map('map', 'mapbox.streets');
 
+        var geocoderOptions = {
+          bounds: true,
+          markers: false,
+          layers: 'coarse',
+          expanded:true
+        }
 
+        var geocoder = L.control.geocoder('search-R7-i3bQ',geocoderOptions).addTo(map);
+        var geocontainer = document.getElementById("geocontainer")
+        geocontainer.appendChild(geocoder.getContainer());
+
+        geocoder.on("select", function(e){
+          console.log(e)
+        })
 
       }
     }

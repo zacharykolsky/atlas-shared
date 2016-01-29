5972e264f14eb1c938f3cdab629f20263e77d077
Tyler Crosse <tylerscottcrosse@gmail.com>
2016-01-28 20:25:49 -0500

    More tweaks

--- public/js/app.js
+++ public/js/app.js
@@ -1,46 +1,46 @@
 "use strict";
 
-(function(){
+(function() {
   angular
-  .module("atlas", [
-    "ui.router",
-    "trips",
-    "locations",
-    "protips",
-    "users"
-  ])
-  .config([
-    "$stateProvider",
-    "$locationProvider",
-    RouterFunction
-  ]);
+    .module("atlas", [
+      "ui.router",
+      "trips",
+      "locations",
+      "protips",
+      "users"
+    ])
+    .config([
+      "$stateProvider",
+      "$locationProvider",
+      RouterFunction
+    ]);
 
-  function RouterFunction($stateProvider,$locationProvider){
+  function RouterFunction($stateProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
     $stateProvider
-    .state("userIndex", {
-      url: "/profile",
-      templateUrl: "js/users/index.html",
-      controller: "UserIndexController",
-      controllerAs: "UserIndexViewModel"
-    })
-    .state("userShow", {
-      url: "/users/:id",
-      templateUrl: "js/users/show.html",
-      controller: "UserShowController",
-      controllerAs: "UserShowViewModel"
-    })
-    .state("tripsIndex", {
-      url: "/trips",
-      templateUrl: "js/trips/index.html",
-      controller: "TripIndexController",
-      controllerAs: "TripIndexViewModel"
-    })
-    .state("tripsShow", {
-      url: "/trips/:id",
-      templateUrl: "js/trips/show.html",
-      controller: "TripShowController",
-      controllerAs: "TripShowViewModel"
-    })
+      .state("userIndex", {
+        url: "/profile",
+        templateUrl: "js/users/index.html",
+        controller: "UserIndexController",
+        controllerAs: "UserIndexViewModel"
+      })
+      .state("userShow", {
+        url: "/users/:id",
+        templateUrl: "js/users/show.html",
+        controller: "UserShowController",
+        controllerAs: "UserShowViewModel"
+      })
+      .state("tripsIndex", {
+        url: "/trips",
+        templateUrl: "js/trips/index.html",
+        controller: "TripIndexController",
+        controllerAs: "TripIndexViewModel"
+      })
+      .state("tripsShow", {
+        url: "/trips/:id",
+        templateUrl: "js/trips/show.html",
+        controller: "TripShowController",
+        controllerAs: "TripShowViewModel"
+      })
   }
 }());
